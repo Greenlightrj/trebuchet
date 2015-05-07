17daeeeb4ddbb4ed1d2f2a7398ea3abf6d3c2a57
SeanCCarter <seancarter26@gmail.com>
2015-05-07 19:13:15 -0400

    Added another graph.

--- grapher.m
+++ grapher.m
@@ -1,19 +1,33 @@
 function res = grapher()
 
+%     V = [];
+%     M_m = [];
+%     counter = 1;
+%     for i = 1:.2:90
+%         V(counter) = -lever_arm_calculation(5, 5, i, 100-i)*5;
+%         M_m(counter) = i/(100-i);
+%         counter = counter + 1;
+%     end
+%     
+%     display('You should be seing a graph right about now.')
+%     plot(M_m, V, 'Linewidth', 3)
+%     set(gca, 'FontSize', 18)
+%     xlabel('Mass Ratio (M/m)', 'FontSize', 16)
+%     ylabel('Launch Velocity (m/s)', 'fontsize', 16)
+%     title('Mass Ratio vs. Launch Velocity', 'FontSize', 24)
+%     display('Yep. Right now.')
+    
     V = [];
-    M_m = [];
+    p_p = [];
     counter = 1;
-    for i = 1:.2:90
-        V(counter) = -lever_arm_calculation(5, 5, i, 100-i)*5;
-        M_m(counter) = i/(100-i);
+    for i = .111111:.1:9.999999
+        V(counter) = -lever_arm_calculation(i, 10-i, 1, 10)*i;
+        p_p(counter) = i;
         counter = counter + 1;
     end
     
-    display('You should be seing a graph right about now.')
-    plot(M_m, V, 'Linewidth', 3)
-    set(gca, 'FontSize', 18)
+    plot(p_p, V, 'Linewidth', 3)
     xlabel('Mass Ratio (M/m)', 'FontSize', 16)
     ylabel('Launch Velocity (m/s)', 'fontsize', 16)
-    title('Mass Ratio vs. Launch Velocity', 'FontSize', 24)
-    display('Yep. Right now.')
+    title('Beam Ratio vs. Launch Velocity', 'FontSize', 24)
 end
--- lever_arm.m
+++ lever_arm.m
@@ -1,6 +1,6 @@
 function res = lever_arm()
 	L1 = 5; % meters length of counterweight arm
-    L2 = 5; % meters length of other arm
+    L2 = 0; % meters length of other arm
     M = 10; % kg mass of counterweight
     m = 1; % kg mass of thrown thing
     g = 9.8; % m/s^2 gravity
@@ -10,8 +10,9 @@ function res = lever_arm()
     dtheta0 = 0;
     initial = [theta0; dtheta0];
     
-    [t, M] = ode45(@derivatives,[0,10],initial);
+    [t, M] = ode45(@derivatives,[0,100],initial);
     animate(t, M)
+    
     
     function res = derivatives(t, V)
         theta = V(1);
@@ -22,7 +23,7 @@ function res = lever_arm()
     end
 
     function res = ddtheta_calc(theta, dtheta)
-        numerator = -M*g*L1*cos(theta) - m*g*L2*cos(theta);
+        numerator = -M*g*L1*cos(theta) - m*g*L2*sin(theta);
         denominator = M*L1^2 + m*L2^2;
         res = numerator/denominator;
     end
